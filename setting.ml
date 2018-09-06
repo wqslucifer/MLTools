8c1b3aea644f1a380a8a8c0a2690ce6090c58ce6
QS <qslucifer@gmail.com>
2018-09-06 16:58:35 -0700

    clean

--- setting.ml
+++ setting.ml
@@ -1,1 +1,1 @@
-{"projectOpenHistory": {"MLTest": ["Wed Sep  5 16:29:02 2018", "E:\\project\\MLTest", "E:\\project\\MLTest\\MLTest.mlproj"]}}
+{"projectOpenHistory": {"MLTest": ["Thu Sep  6 16:53:22 2018", "E:\\project\\MLTest", "E:\\project\\MLTest\\MLTest.mlproj"]}}
